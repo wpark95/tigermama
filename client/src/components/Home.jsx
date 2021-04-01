0350dec0b3dd3dfe3f3de200ff5e057adc577d62
Will <park.wongyun@gmail.com>
2021-03-31 19:37:16 -0700

    Fix video maximize issue in iOS

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -6,8 +6,8 @@ import {
 
 const home = () => (
   <div className="body-container body-container-home">
-    <video id="main-video" loop autoPlay muted >
-      <source src="exampleVideo.mp4" type="video/mp4"/>
+    <video id="main-video" loop autoPlay muted playsInline >
+      <source src="exampleVideo.mp4" type="video/mp4" />
     </video>
     <h1>Tiger Mama Burgers</h1>
     <h2>921 W Commerce St Dallas, TX 75208</h2>
--- client/src/components/OrderOnline.jsx
+++ client/src/components/OrderOnline.jsx
@@ -7,6 +7,9 @@ import {
 const orderOnline = () => (
   <div className="body-container body-container-order">
     <h1>Order Tiger Mama Online!</h1>
+    <p>Uber Eats Icon</p>
+    <p>Postmates Icon</p>
+    <p>Doordash Icon</p>
   </div>
 );
 
