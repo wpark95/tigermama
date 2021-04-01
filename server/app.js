2a4ab8ce10b6a0dafcc0f7850faa7feb5d9eca9c
Will <park.wongyun@gmail.com>
2021-03-31 18:07:30 -0700

    Fix typo in server.js

--- server/app.js
+++ server/app.js
@@ -12,18 +12,14 @@ app.use(express.static(DIST_DIR));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+
 // ***** Redirect All Server Requests to index.html
-app.get('/*', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(indexHTML, (err) => {
     if (err) {
       res.status(500).send(err);
     }
   });
-});
-
-app.post('/test/textinput', (req, res) => {
-  console.log(req.body);
-  res.status(201).send();
 });
 
 module.exports = app;
